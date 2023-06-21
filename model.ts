ec3890bf2182858344c9c16b34e998ce0a9db00c
sagveakshay <101961826+sagveakshay@users.noreply.github.com>
2023-06-21 17:13:19 +0530

    Add files via upload

---
+++ model.ts
@@ -0,0 +1,12 @@
+export class Rootobject{
+    // values:Array<any>=[]
+    AWS_IAM_User:any
+    Event_Name:any
+    // User_Agent:any
+    // AWS_S3_Bucket_Name:any
+    // Folder:any
+    // Year:any
+    // Month :any
+    // Day:any
+
+}
